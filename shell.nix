274b401d3a7057f81852c01cf8a5f81474c2966c
Dominik Polakovics <dominik.polakovics@cloonar.com>
2023-10-03 13:49:34 +0200

    add darwin

--- shell.nix
+++ shell.nix
@@ -1,12 +1,16 @@
 with import <nixpkgs> {};
 let
   nix_pkgs_with_hugo_106 = import (builtins.fetchGit {
-         # Descriptive name to make the store path easier to identify                
-         name = "nix_pkgs_with_singularity";                                                 
-         url = "https://github.com/NixOS/nixpkgs/";                       
-         ref = "refs/heads/nixos-22.11";                     
-         rev = "6adf48f53d819a7b6e15672817fa1e78e5f4e84f";                                           
+         # Descriptive name to make the store path easier to identify
+         name = "nix_pkgs_with_singularity";
+         url = "https://github.com/NixOS/nixpkgs/";
+         ref = "refs/heads/nixos-22.11";
+         rev = "6adf48f53d819a7b6e15672817fa1e78e5f4e84f";
      }) {};
+
+  os = if pkgs.system == "x86_64-darwin"
+    then "darwin"
+    else "linux";
 in stdenv.mkDerivation {
     name = "node";
     buildInputs = [
@@ -16,13 +20,13 @@ in stdenv.mkDerivation {
     ];
     shellHook = ''
         export PATH="$PWD/node_modules/.bin/:$PATH"
-        mkdir -p $PWD/node_modules/.bin/hugo 
+        mkdir -p $PWD/node_modules/.bin/hugo
         ln -s ${nix_pkgs_with_hugo_106.hugo}/bin/hugo $PWD/node_modules/.bin/hugo/hugo
         cat <<EOT > $PWD/node_modules/.bin/hugo/version.json
 {
   "arch": "x64",
   "extended": true,
-  "os": "linux",
+  "os": "${os}",
   "version": "0.106.0"
 }
 EOT
